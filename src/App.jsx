ff7791fd91aa1197ebac90589fd5af4cebe6cc07
shyamtawli <shyamtawli2@gmail.com>
2023-06-28 20:41:32 +0530

    Pages and route set

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-// import { Switch, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
 
 function App() {
   return (
@@ -8,7 +16,23 @@ function App() {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
       <div className="footer"></div>
     </div>
   );
