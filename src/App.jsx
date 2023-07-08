df4c712527b829afa0ae64b4175ecb8c3075476b
shyamtawli <shyamtawli2@gmail.com>
2023-07-08 13:22:30 +0530

    footer
    Signed-off-by: shyamtawli <shyamtawli2@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -32,8 +32,21 @@ function App() {
             </Routes>
           </div>
         </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            CryptoVerse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 }
