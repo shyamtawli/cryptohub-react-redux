a4c633c2ddbe5deba50cd4a9741b5ff56759b3ab
shyamtawli <shyamtawli2@gmail.com>
2023-07-15 20:02:30 +0530

    Select Option : News Category

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment/moment";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsAPI";
+import { useGetCryptosQuery } from "../services/cryptoAPI";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
 
 function News({ simplified }) {
-  const { data } = useGetCryptoNewsQuery({
-    newsCategory: "Cryptocurrency",
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const { data } = useGetCryptosQuery(100);
+  const { data: crytpoNews } = useGetCryptoNewsQuery({
+    newsCategory,
     count: simplified ? 6 : 12,
   });
 
-  if (!data) return "Loading...";
-  console.log(data);
+  if (!crytpoNews?.value) return "Loading...";
+  // console.log(data);
 
   return (
     <Row gutter={[24, 24]}>
-      {data.value.map((news, i) => (
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            onChange={(value) => setNewsCategory(value)}
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+            {data?.data.coins.map((coin) => (
+              <Option value={coin.name}>{coin.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
+      {crytpoNews?.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
             <a href={news.url} target="_blank" rel="noreferrer">
